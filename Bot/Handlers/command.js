38600cbd7df3b8a1925505ec8aff15ad64ceb845
Antoni <diego.padilla135@gmail.com>
2020-07-22 10:20:38 -0400

    More Documentation

--- Bot/Handlers/command.js
+++ Bot/Handlers/command.js
@@ -5,15 +5,20 @@ const table = new ascii().setHeading("Command", "Load Status");
 
 //Creating the Table that is displayed when the bot is ran
 module.exports = client => {
-    //Reads the Directories and Files in the Commands Directory
+    //Reads the Directories in the Commands Directory
     readdirSync("./Bot/Commands/").forEach(dir => {
+
+        //Commands variable reads the directories files and filter (only gets) the ones ending with .js
         const commands = readdirSync(`./Bot/Commands/${dir}/`).filter(f =>
             f.endsWith(".js")
         );
 
         for(let file of commands) {
+            //Pull gets the files
             let pull = require(`../Commands/${dir}/${file}`);
+            //Creates new pull
             let command = new pull();
+            //If command.name exists, Set command name in collection, as well as command, and add new row with the file variable, otherwise add x
             if(command.name) {
                 client.commands.set(command.name, command);
                 table.addRow(file, `✅`);
@@ -22,6 +27,7 @@ module.exports = client => {
                 continue;
             }
 
+            //If Command Aliases exist and its an array, Set aliases in collection, as well as the command name
             if(command.aliases && Array.isArray(command.aliases)) {
                 command.aliases.forEach(alias =>
                     client.aliases.set(alias, command.name));
